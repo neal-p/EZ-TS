523e8426ea79994bcac27caaea87306ff4317701
neal-p <58431101+neal-p@users.noreply.github.com>
2020-04-20 08:54:45 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,4 @@ optmethod="hf"
 optbasis="6-31G"
 #remeber force constants required for ts optimization
 optroute="opt=(ts,noeigen,calcfc) freq=noraman"
-user='neal.pa@husky.neu.edu'
 opttime='23:50:00'
-
-#conf_opt  conf_search  input  lowest_ts  ts_guess  utilities
-
-
