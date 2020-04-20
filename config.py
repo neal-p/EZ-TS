89a5ab6706596ba7ec17b7aa348f1e8a82f15e79
neal-p <58431101+neal-p@users.noreply.github.com>
2020-04-20 16:30:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Input Parameters
 optcores="7"
 optmemory="28" #GB
-optpartition="lopez"
-optmethod="hf"
+optpartition="short,lopez"
+optmethod="b3lyp"
 optbasis="6-31G"
 #remeber force constants required for ts optimization
-optroute="opt=(ts,noeigen,calcfc) freq=noraman"
+optroute="empiricaldispersion=GD3bj opt=(ts,noeigen,calcfc) freq=noraman"
 opttime='23:50:00'
