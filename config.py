b0e74b2655ec65d6a7828424b0ebd0f60abd6982
neal-p <58431101+neal-p@users.noreply.github.com>
2020-04-21 11:28:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ optmethod="b3lyp"
 optbasis="6-31G"
 #remeber force constants required for ts optimization
 optroute="empiricaldispersion=GD3bj opt=(ts,noeigen,calcfc) freq=noraman"
-opttime='23:50:00'
+opttime='1:00:00'
