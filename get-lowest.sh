7f84f06fe146946f04838400857ca46312e7210e
neal-p <58431101+neal-p@users.noreply.github.com>
2020-04-21 11:18:57 -0400

    Update get-lowest.sh

--- get-lowest.sh
+++ get-lowest.sh
@@ -1,7 +1,5 @@
 #!bin/bash
-for n in ../input/*log
-    do 
-    search=$(basename ${n%.*})
+    search=$(basename ${1%.*})
     for i in $search*log;
         do
         freq=$(grep "Frequencies" -m1 $i |awk '{ print $3 }' )
@@ -12,19 +10,23 @@ for n in ../input/*log
             echo $i $energy >> $search-ts-energies.txt
         fi
      done
-    lowestenergyts=$(sort -g -k2,2 $search-ts-energies.txt | head -1 |awk '{ print $1 }')
+    if test -f "$search-ts-energies.txt"
+        then
+        lowestenergyts=$(sort -g -k2,2 $search-ts-energies.txt | head -1 |awk '{ print $1 }')
 
 #if this is lowest energy guess_ts, then setup for conformational search, if this is final lowest TS move to lowest_ts
-    if [[ $1 == 'ts_guess' ]]
-        then
-        obabel $lowestenergyts -o mol2 -O ../conf_search/$search.mol2
-        python3 ../utilities/maestro-input.py $lowestenergyts $2 $3 $4 $5 $6
-    elif [[ $1 == 'conf_opt' ]]
-        then
-        cp $lowestenergyts ../lowest_ts/
-        grep "Sum of electronic and thermal Free Energies" ../lowest_ts/$2*log > ../lowest_ts/$2-lowest_ts-energies.txt
-        echo " " >> ../lowest_ts/$2-lowest_ts-energies.txt
-        cat $search-ts-energies.txt >> ../lowest_ts/$2-lowest_ts-energies.txt
-    fi
-done
+        if [[ $1 == 'ts_guess' ]]
+            then
+            obabel $lowestenergyts -o mol2 -O ../conf_search/$search.mol2
+            python3 ../utilities/maestro-input.py $lowestenergyts $3 $4 $5 $6 $7
+        elif [[ $1 == 'conf_opt' ]]
+            then
+            cp $lowestenergyts ../lowest_ts/
+            grep "Sum of electronic and thermal Free Energies" ../lowest_ts/$1*log > ../lowest_ts/$1-lowest_ts-energies.txt
+            echo " " >> ../lowest_ts/$1-lowest_ts-energies.txt
+            cat $search-ts-energies.txt >> ../lowest_ts/$1-lowest_ts-energies.txt
+        fi
+    else
+         echo "No valid TS found" >> $search-ts-energies.txt
+fi
 
