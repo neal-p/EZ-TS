3ba88da8508f25df0b1a8e081e78f8affec2967d
neal-p <58431101+neal-p@users.noreply.github.com>
2021-01-04 10:32:25 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cp autots-setup.sh ~/bin/autots-setup
-chmod 777 ~/bin/autots-setup
+cd ~/EZTS
+cp EZTS-setup.sh ~/bin/EZTS-setup
+chmod 777 ~/bin/EZTS-setup
 cp re-configure.sh ~/bin/re-configure
 chmod 777 ~/bin/re-configure
-cp autots-update.sh ~/bin/autots-update
-chmod 777 ~/bin/autots-update
+cp EZTS-update.sh ~/bin/EZTS-update
+chmod 777 ~/bin/EZTS-update
 chmod 777 *sh
-cp autots-clean.sh ~/bin/autots-clean
-chmod 777 ~/bin/autots-clean
-sed -i "/#local workflow variables/i user='$USER@husky.neu.edu'" config.py
+cp EZTS-clean.sh ~/bin/EZTS-clean
+chmod 777 ~/bin/EZTS-clean
+sed -i "/#local workflow variables/i user='$USER@northeastern.neu.edu'" config.py
